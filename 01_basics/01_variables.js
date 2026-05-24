7f5813b0dc237ff0baabdf2bc65b9ad32698d0eb
Vishal Choudhary <vishal.choudhary1716@gmail.com>
2026-05-24 19:03:22 +0000

    Variable practice

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accId = 4223
+let accMail = "vishal@gmail.com"
+var accPhone = 4232323
+// var and let are same but var escape the scope so that is why we only use let
+userState = "Punjab"
+console.log(accId)
+// It is used to show the data in tabular form
+console.table([accId,accMail,accPhone,userState])
