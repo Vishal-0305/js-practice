426c529e420528b0814079c5920e93ec0673a175
Vishal Choudhary <vishal.choudhary1716@gmail.com>
2026-05-25 13:38:29 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// null convert to 0 and equality and comparison behave differently
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+// in this data type is not checked
+console.log("2"==2)
+
+// in this data type is also checked
+console.log("2"===2)
