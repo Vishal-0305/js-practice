26f82285f1b1a3641294043aeb707ee687c8173c
Vishal Choudhary <vishal.choudhary1716@gmail.com>
2026-05-25 09:46:57 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+let name = "Vishal"
+console.log(name)
+
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol = unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
